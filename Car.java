3948110944cd05fcdda0443dc0a2d6f4b42bb784
nassorishak <nassorishak0@gmail.com>
2023-01-07 01:30:56 -0800

    m

---
+++ Car.java
@@ -0,0 +1,19 @@
+public class Car {
+    int speed;
+    double regularPrice;
+    String color;
+int saleprice;
+public Car(int speed,double regularPrice,String color){
+    this.speed=speed;
+    this.regularPrice=regularPrice;
+    this.color=color;
+           }
+
+public int getSaleprice() {
+    return saleprice;
+}
+
+   
+    
+
+}
